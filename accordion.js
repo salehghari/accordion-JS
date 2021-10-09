be1c362a79a6e759725a91b1d9b7d94d1fcd1d74
salehghari <salehghari8470@gmail.com>
2021-10-09 20:42:53 +0330

    Add more accordions

--- accordion.js
+++ accordion.js
@@ -1,25 +1,27 @@
-let accordion = document.querySelector(".accordion");
+let accordions = document.querySelectorAll(".accordion");
 
-Array.from(accordion.children).forEach(wrapper => {
-    if(wrapper.classList.contains("show")) {
-        wrapper.querySelector("div").style.maxHeight = wrapper.querySelector("div").scrollHeight + 30 + "px"
-    }
-    wrapper.querySelector("span").addEventListener("click", e => {
-        let span = e.target
-        let wrapper = span.parentElement
-        let div = span.nextElementSibling
-        wrapper.classList.toggle("show")
-        if(wrapper.classList.contains("show")){
-            div.style.maxHeight = div.scrollHeight + 30 + "px"
+accordions.forEach(accordion => {
+    Array.from(accordion.children).forEach(wrapper => {
+        if(wrapper.classList.contains("show")) {
+            wrapper.querySelector("div").style.maxHeight = wrapper.querySelector("div").scrollHeight + 30 + "px"
         }
-        else{
-            div.style.maxHeight = null
-        }
-        Array.from(accordion.children).forEach(w => {
-            if(w != wrapper){
-                w.classList.remove("show")
-                w.querySelector('div').style.maxHeight = null
+        wrapper.querySelector("span").addEventListener("click", e => {
+            let span = e.target
+            let wrapper = span.parentElement
+            let div = span.nextElementSibling
+            wrapper.classList.toggle("show")
+            if(wrapper.classList.contains("show")){
+                div.style.maxHeight = div.scrollHeight + 30 + "px"
             }
+            else{
+                div.style.maxHeight = null
+            }
+            Array.from(accordion.children).forEach(w => {
+                if(w != wrapper){
+                    w.classList.remove("show")
+                    w.querySelector('div').style.maxHeight = null
+                }
+            })
         })
     })
 })
